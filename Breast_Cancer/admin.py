636f730df5c6df6d8c0ab6d448012897ffe35b34
saksham <everydaycodings@gmail.com>
2020-09-14 21:31:29 +0530

    edited data model

--- Breast_Cancer/admin.py
+++ Breast_Cancer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Data
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
+@admin.register(Data)
+class Add_friendAdmin(ImportExportModelAdmin):
+   list_display = ("clump_thickness", "result")
+   list_filter = ("result",)
--- Breast_Cancer/models.py
+++ Breast_Cancer/models.py
@@ -3,17 +3,21 @@ from django.db import models
 # Create your models here.
 OPTION = (
     (1,1),
-    (2,2)
-    (3,3)
-    (4,4)
-    (5,5)
-    (6,6)
-    (7,7)
-    (8,8)
-    (9,9)
+    (2,2),
+    (3,3),
+    (4,4),
+    (5,5),
+    (6,6),
+    (7,7),
+    (8,8),
+    (9,9),
     (10,10)
 )
 
+RESULT = (
+    (0, "Cancer"),
+    (1, "Non-Cancer")
+)
 
 class Data(models.Model):
     clump_thickness = models.IntegerField("Clump Thickness", choices=OPTION)
@@ -25,3 +29,7 @@ class Data(models.Model):
     bland_chromatin = models.IntegerField("Bland Chromatin", choices=OPTION)
     normal_nucleoli = models.IntegerField("Normal Nucleoli", choices=OPTION)
     mitoses = models.IntegerField("Mitoses", choices=OPTION)
+    result = models.IntegerField("Result", choices=RESULT, default=1)
+
+    def __str__(self):
+        return self.clump_thickness
