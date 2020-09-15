c4709a987ec61051ead363de2db9194bd22281d7
saksham <everydaycodings@gmail.com>
2020-09-15 12:19:11 +0530

    made cancer_model function in terminal in views.py

--- Breast_Cancer/views.py
+++ Breast_Cancer/views.py
@@ -5,34 +5,38 @@ import math
 
 def index(request):
 
-
     pickle_in = open("Breast_Cancer/Cancer_model.pickle", "rb")
     linear = pickle.load(pickle_in)
 
-    print("All The Values Will Be Between 1-10")
-    clump_thickness = int(input("Clump_Thickness: "))
-    cell_size = int(input("Cell_Size: "))
-    cell_shape = int(input("Cell_Shape: "))
-    marginal_adhesion = int(input("Marginal_Adhesion: "))
-    s_e_c_s = int(input("single_Epithelial_Cell_Size: "))
-    nuclei = int(input("Bare_Nuclei: "))
-    chromatin = int(input("Bland_Chromatin: "))
-    n_nucleoli = int(input("Normal_Nucleoli: "))
-    mitoses = int(input("Mitoses: "))
+    context = {}
 
-    customValue = [
-        [clump_thickness, cell_size, cell_shape, marginal_adhesion, s_e_c_s, nuclei, chromatin, n_nucleoli, mitoses]]
+    if request.method == "POST":
+        clump_thickness = int(request.POST["clump_thickness"])
+        cell_size = int(request.POST["cell_size"])
+        cell_shape = int(request.POST["cell_shape"])
+        marginal_adhesion = int(request.POST["marginal_adhesion"])
+        s_e_c_s = int(request.POST["s_e_c_s"])
+        nuclei = int(request.POST["nuclei"])
+        chromatin = int(request.POST["chromatin"])
+        n_nucleoli = int(request.POST["n_nucleoli"])
+        mitoses = int(request.POST["mitoses"])
 
-    predictions = linear.predict(customValue)
-    final_prediction = math.trunc(int(predictions))
+        customValue = [
+            [clump_thickness, cell_size, cell_shape, marginal_adhesion, s_e_c_s, nuclei, chromatin, n_nucleoli, mitoses]]
 
-    print()
-    if final_prediction == 0:
-        print("Not Having Cancer")
-        print("Predicted Value:", predictions)
-    if final_prediction == 1:
-        print("Having Cancer")
-        print("Predicted_Value: ", predictions)
+        predictions = linear.predict(customValue)
+        final_prediction = math.trunc(int(predictions))
+        print(final_prediction)
+
+        if final_prediction == 0:
+            print("Not Having Cancer")
+            print("Predicted Value:", predictions)
+        if final_prediction == 1:
+            print("Having Cancer")
+            print("Predicted_Value: ", predictions)
+
+        context = {"predictions": predictions, "final_prediction": final_prediction}
 
 
+        
     return render (request, "Breast_Cancer/index.html")
